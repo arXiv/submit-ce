6355ff2faafac628c37dc9516b950edf4426f1d3
Brian D. Caruso <bdc34@cornell.edu>
2024-09-25 17:52:25 -0400

    Adds a main_ui.py

---
+++ main_ui.py
@@ -0,0 +1,15 @@
+"""Simple file to run submit ui in debug mode.
+
+Run as `python main.py`"""
+import os
+from submit_ce.ui.factory import create_web_app
+
+if __name__ == "__main__":
+    os.environ['TEMPLATES_AUTO_RELOAD'] = "1"
+    os.environ['DEBUG'] = "1"
+    app = create_web_app()
+    if os.environ.get('PROFILE', False):
+        from werkzeug.middleware.profiler import ProfilerMiddleware
+        app.wsgi_app = ProfilerMiddleware(app.wsgi_app, profile_dir="./profs")
+        print("WARNING: Profiling, will be slower. Writing to ./profs ")
+    app.run(debug=True, port=8080)
