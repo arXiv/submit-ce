e0ca51b09192aaef57023252b24b04ddcac50dc8
Brian D. Caruso <bdc34@cornell.edu>
2025-07-23 19:37:12 -0400

    Adds handling of current arxiv.org/login NG JWT

--- submit_ce/ui/auth.py
+++ submit_ce/ui/auth.py
@@ -1,15 +1,19 @@
+from datetime import datetime, timezone
 import logging
 from typing import Callable, Tuple, Optional
+
+import jwt
 
 from arxiv.auth.legacy import util
 from arxiv.db.models import Demographic, TapirNickname, TapirUser
 from arxiv.db import Session as DB  # renamed due to too many session
 from flask import has_app_context, has_request_context, request
+from pydantic_core import ValidationError
 from werkzeug.datastructures import MultiDict
 from werkzeug.exceptions import Unauthorized, NotFound
 from werkzeug.http import parse_cookie
 
-from arxiv.auth.auth import tokens
+from arxiv.auth.auth.tokens import decode
 from arxiv.auth.auth.exceptions import ExpiredToken, InvalidToken, MissingToken, SessionCreationFailed
 from arxiv.auth import domain as auth_domian
 from arxiv.auth.legacy.endorsements import explicit_endorsements
@@ -20,13 +24,26 @@ from submit_ce.ui import backend, get_device_type, is_admin
 from submit_ce.ui.config import settings
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+def _to_datetime(time:str|int|datetime|None)-> datetime:
+    if isinstance(time, datetime):
+        return time
+    if isinstance(time, str) and time:
+        return datetime.fromisoformat(time)
+    if isinstance(time, int):
+        return datetime.fromtimestamp(time, tz=timezone.utc)
+    else:
+        return datetime.fromtimestamp(0, tz=timezone.utc)
 
 
-def _ip_address(environ) -> str:
+def _ip_address() -> str:
+    environ = request.environ
     try:
         return environ['HTTP_X_FORWARDED_FOR'].split(',')[-1].strip()
     except KeyError:
-        return environ['REMOTE_ADDR'] or "unknown"
+        return getattr(environ,'REMOTE_ADDR', "unknown")
+
 
 def _get_cookies(environ) -> list[str]:
     """Get all cookies with key ARXIVNG_SESSION_ID."""
@@ -44,31 +61,17 @@ def _get_auth_bearer(environ) -> list[str]:
         return [bearer.strip().removeprefix("Bearer ").strip()]
 
 
-def _get_first_valid_jwt(secret, cookies) -> Tuple[auth_domian.Session, str]:
-    """Get the first cookie that decodes as a JWT with the secret."""
-    if not cookies:
-        raise MissingToken
-    
-    for cookie in cookies:
-        try:    
-            jwt_data = tokens.decode(cookie, secret)
-            if jwt_data:
-                return jwt_data, cookie
-        except(InvalidToken):
-            continue
-
-    raise InvalidToken(f"Only invalid tokens found in {len(cookies)}")
-    
-def _session_from_db(jwt: auth_domian.Session) -> auth_domian.Session:
-    """Gets a user from the db.
+def _session_from_db(user_id, session_id,
+                     end_iso: str|int|datetime, start_iso: str|int|datetime = "",
+                     ip: Optional[str] = None) -> auth_domian.Session:
+    """Gets a `arxiv.auth.domian.Session` from the db.
 
     Uses endorsements and scopes from db, not from jwt since jwt seems not record these.
     """
-    if not jwt or not jwt.user or not jwt.user.user_id:
-        raise SessionCreationFailed(f"no session or no user. ip {jwt.ip_address}")
-    user_id = jwt.user.user_id
-    if jwt.expired:
-        raise ExpiredToken(f'JWT has an expired session {jwt.session_id} user {user_id} ip {jwt.ip_address}')
+    expires = _to_datetime(end_iso)
+    start = _to_datetime(start_iso)
+    if expires < datetime.now(tz=timezone.utc):
+        raise ExpiredToken(f'JWT has an expired session {session_id} user {user_id}')
 
     data: Tuple[TapirUser, TapirNickname, Demographic] = \
     DB.query(
@@ -76,11 +79,10 @@ def _session_from_db(jwt: auth_domian.Session) -> auth_domian.Session:
           .join(TapirNickname).join(Demographic) \
           .filter(TapirUser.user_id == user_id) \
           .first()
-
     if not data or len(data) != 3 or not data[0]:
         raise SessionCreationFailed(f'No such user {user_id}')
-    db_user, db_nick, db_profile = data
 
+    db_user, db_nick, db_profile = data
     user = auth_domian.User(
         user_id=str(user_id),
         username=db_nick.nickname,
@@ -97,24 +99,128 @@ def _session_from_db(jwt: auth_domian.Session) -> auth_domian.Session:
         classic=util.compute_capabilities(db_user),
         scopes=util.get_scopes(db_user)
     )
-    db_session = auth_domian.Session(session_id=jwt.session_id,
-                                       start_time=jwt.start_time, end_time=jwt.end_time,
-                                       user=user, authorizations=authorizations)
-    logger.debug('loaded user %s', db_session.user.user_id)
-    db_session.ip_address = jwt.ip_address
+
+    db_session = auth_domian.Session(user=user, session_id=session_id,
+                                     start_time=start, end_time=expires,
+                                     authorizations=authorizations)
+    logger.debug('loaded user %s', user_id)
+    db_session.ip_address = ip if ip else _ip_address()
     return db_session
+    
 
+def _modern_auth(tokens: list[str]) -> Tuple[auth_domian.Session, str]:
+    """Try to deocde jwt to `auth_domain.Session` using code from `arxiv.auth.auth.tokens`.
 
+    This is the kind of code we should move to once auth if finished."""
+    if not tokens:
+        raise MissingToken()
+    session, token = None, None
+    for jwt_orig in tokens:
+        try:    
+            jwt_data = decode(jwt_orig, settings.JWT_SECRET)
+            if jwt_data:
+                session, token = jwt_data, jwt_orig
+                break
+        except(InvalidToken):
+            continue
+    if not session or not token or not session.user or not session.user.user_id:
+        raise Unauthorized("no invalid token or cookie")
+
+    user_id = session.user.user_id
+    if session.expired:
+        raise ExpiredToken(f'JWT has an expired session {session.session_id} user {user_id}'\
+                           f'ip {session.ip_address}')
+
+    db_session = _session_from_db(session.user.user_id, session.session_id,
+                                  session.end_time, session.start_time,
+                                  session.ip_address)
+    return db_session, token
+
+    
+def _ng_dict_jwt_auth(tokens: list[str]) -> Tuple[auth_domian.Session, str]:
+    """Try to decode jwt just to a dict and use that to auth.
+
+    This code should be retired once keycloak and related services are working.
+
+    NG loggin doesn't seem to make a full JWT so the arxiv.auth.auth.tokens fails
+    with a pydantic error.
+
+    Example data:
+
+       {
+         'user_id': '1234',
+         'session_id': '596b995b-7907-411e-9551-375f95c471d2',
+         'nonce': '22307705',
+         'expires': '2025-07-26T19:42:22.218281+00:00'
+        }
+    """
+    if not tokens:
+        raise MissingToken()
+    session, token = None, None
+    for jwt_orig in tokens:
+        try:
+            jwt_data = dict(jwt.decode(jwt_orig, settings.JWT_SECRET, algorithms=['HS256']))
+            if jwt_data:
+                session, token = jwt_data, jwt_orig
+        except jwt.DecodeError:
+            continue
+    if (not session or not token or "user_id" not in session or "session_id"
+        not in session or "expires" not in session):
+        raise Unauthorized("no invalid token or cookie")
+
+    user_id = session["user_id"]
+    session_id = session["session_id"]
+    expires = datetime.fromisoformat(session["expires"])
+    if expires < datetime.now(tz=timezone.utc):
+        raise ExpiredToken(f'JWT has an expired session {session_id} user {user_id}')
+
+    data: Tuple[TapirUser, TapirNickname, Demographic] = \
+    DB.query(
+          TapirUser, TapirNickname, Demographic) \
+          .join(TapirNickname).join(Demographic) \
+          .filter(TapirUser.user_id == user_id) \
+          .first()
+    if not data or len(data) != 3 or not data[0]:
+        raise SessionCreationFailed(f'No such user {user_id}')
+
+    db_user, db_nick, db_profile = data
+    user = auth_domian.User(
+        user_id=str(user_id),
+        username=db_nick.nickname,
+        email=db_user.email,
+        name=auth_domian.UserFullName(
+            forename=db_user.first_name or "",
+            surname=db_user.last_name or "",
+            suffix=db_user.suffix_name
+        ),
+        profile=auth_domian.UserProfile.from_orm(db_profile) if db_profile else None,
+        verified=bool(db_user.flag_email_verified)
+    )
+    authorizations = auth_domian.Authorizations(
+        classic=util.compute_capabilities(db_user),
+        scopes=util.get_scopes(db_user)
+    )
+    epoch_start = datetime.fromtimestamp(0, tz=timezone.utc)
+    db_session = auth_domian.Session(session_id=session_id,
+                                       start_time=epoch_start,
+                                       end_time=expires,
+                                       user=user, authorizations=authorizations)
+    logger.debug('loaded user %s', user_id)
+    db_session.ip_address = _ip_address()
+    return db_session, token
     
 def setup_auth():
     """For use with `@app.before_reqeust()` to add auth attributes to `request`.
 
     Must be run inside a flask request context."""
+    tokens = _get_cookies(request.environ) + _get_auth_bearer(request.environ)
+    session, jwt_orig = None, None
     try:
-        session, token = _get_first_valid_jwt(settings.JWT_SECRET,
-                                          _get_cookies(request.environ) + _get_auth_bearer(request.environ))
-        request.environ['token'] = token  # Attach which token decrypted for use in sub requests
-        setattr(request,"auth", _session_from_db(session))
+        try:
+            session, jwt_orig = _modern_auth(tokens)
+        except ValidationError as ve:
+            logger.debug(ve)
+            session, jwt_orig = _ng_dict_jwt_auth(tokens)
     except MissingToken:
         raise Unauthorized("no token or cookie")
     except InvalidToken:
@@ -123,6 +229,12 @@ def setup_auth():
         raise Unauthorized("no session or user")
     except ExpiredToken:
         raise Unauthorized("expired")
+
+    if session:
+        setattr(request,"auth", session)
+        request.environ['token'] = jwt_orig
+    else:
+        raise Unauthorized("Authorization failed.")
 
 
 def get_endorsements(user: auth_domian.User) -> list[str]:
--- submit_ce/ui/tests/test_auth.py
+++ submit_ce/ui/tests/test_auth.py
@@ -1,3 +1,24 @@
-def test_auth(authorized_client):
+from submit_ce.ui.auth import _modern_auth, _ng_dict_jwt_auth
+from jwt import encode
+
+def test_modern_auth(app, authorized_user_session, jwt_secret):
+    session, jwt = authorized_user_session
+    with app.test_request_context("/"):
+        db_session, _ = _modern_auth([jwt])
+        assert db_session and db_session.session_id == session.session_id
+
+def test_modern_auth_via_http(authorized_client):
     resp = authorized_client.get("/")
     assert resp.status_code == 200
+
+def test_ng_auth(app, authorized_user_session, jwt_secret):
+    session, _ = authorized_user_session
+    with app.test_request_context("/"):
+        old_style_jwt = encode({
+            'user_id': session.user.user_id,
+            'session_id': session.session_id,
+            'nonce': 'notused',
+            'expires': session.end_time.isoformat(),
+        }, jwt_secret)
+        db_session, _ = _ng_dict_jwt_auth([old_style_jwt])
+        assert db_session and db_session.session_id == session.session_id
