024ba2e9f8b6c820d8edfc3ac3fe122932de7529
Brian D. Caruso <bdc34@cornell.edu>
2025-10-02 15:40:01 -0400

    Change to `make_test_db.py bootstrap_db` to always output jwt

--- submit_ce/make_test_db.py
+++ submit_ce/make_test_db.py
@@ -1,5 +1,6 @@
 """Bootstraps users and other DB entities for testing/dev."""
 
+from atexit import register
 from arxiv.auth.legacy import accounts
 from arxiv.auth.legacy.sessions import create
 from arxiv.base import Base
@@ -38,101 +39,137 @@ logger = logging.getLogger(__file__)
 
 LOCALES = list(Locale)
 
+DEFAULT_AUTHS = domain.Authorizations(
+    classic=0,
+    scopes=[
+        "public:read",
+        "profile:update",
+        "profile:read",
+        "submission:create",
+        "submission:update",
+        "submission:read",
+        "submission:delete",
+        "compile:read",
+        "compile:create",
+        "upload:read",
+        "upload:update",
+        "upload:delete",
+        "preview:read",
+        "preview:create",
+    ],
+)
+
 LICENSES: List[Dict[str, Any]] = [
     {
         "name": "",
         "note": None,
         "label": "None of the above licenses apply",
         "active": 1,
-        "sequence": 99
+        "sequence": 99,
     },
     {
         "name": "http://arxiv.org/licenses/assumed-1991-2003/",
         "note": "",
-        "label": "Assumed arXiv.org perpetual, non-exclusive license to" +
-                 " distribute this article for submissions made before" +
-                 " January 2004",
+        "label": "Assumed arXiv.org perpetual, non-exclusive license to"
+        + " distribute this article for submissions made before"
+        + " January 2004",
         "active": 0,
-        "sequence": 9
+        "sequence": 9,
     },
     {
         "name": "http://arxiv.org/licenses/nonexclusive-distrib/1.0/",
-        "note": "(Minimal rights required by arXiv.org. Select this unless" +
-                " you understand the implications of other licenses.)",
-        "label": "arXiv.org perpetual, non-exclusive license to distribute" +
-                 " this article",
+        "note": "(Minimal rights required by arXiv.org. Select this unless"
+        + " you understand the implications of other licenses.)",
+        "label": "arXiv.org perpetual, non-exclusive license to distribute"
+        + " this article",
         "active": 1,
-        "sequence": 1
+        "sequence": 1,
     },
     {
         "name": "http://creativecommons.org/licenses/by-nc-sa/3.0/",
         "note": "",
-        "label": "Creative Commons Attribution-Noncommercial-ShareAlike" +
-                 " license",
+        "label": "Creative Commons Attribution-Noncommercial-ShareAlike" + " license",
         "active": 0,
-        "sequence": 3
+        "sequence": 3,
     },
     {
         "name": "http://creativecommons.org/licenses/by-nc-sa/4.0/",
         "note": "",
-        "label": "Creative Commons Attribution-Noncommercial-ShareAlike" +
-                 " license (CC BY-NC-SA 4.0)",
+        "label": "Creative Commons Attribution-Noncommercial-ShareAlike"
+        + " license (CC BY-NC-SA 4.0)",
         "active": 1,
-        "sequence": 7
+        "sequence": 7,
     },
     {
         "name": "http://creativecommons.org/licenses/by-sa/4.0/",
         "note": "",
-        "label": "Creative Commons Attribution-ShareAlike license" +
-                 " (CC BY-SA 4.0)",
+        "label": "Creative Commons Attribution-ShareAlike license" + " (CC BY-SA 4.0)",
         "active": 1,
-        "sequence": 6
+        "sequence": 6,
     },
     {
         "name": "http://creativecommons.org/licenses/by/3.0/",
         "note": "",
         "label": "Creative Commons Attribution license",
         "active": 0,
-        "sequence": 2
+        "sequence": 2,
     },
     {
         "name": "http://creativecommons.org/licenses/by/4.0/",
         "note": "",
         "label": "Creative Commons Attribution license (CC BY 4.0)",
         "active": 1,
-        "sequence": 5
+        "sequence": 5,
     },
     {
         "name": "http://creativecommons.org/licenses/publicdomain/",
         "note": "(Suitable for US government employees, for example)",
         "label": "Creative Commons Public Domain Declaration",
         "active": 0,
-        "sequence": 4
+        "sequence": 4,
     },
     {
         "name": "http://creativecommons.org/publicdomain/zero/1.0/",
         "note": "",
         "label": "Creative Commons Public Domain Declaration (CC0 1.0)",
         "active": 1,
-        "sequence": 8
-    }
+        "sequence": 8,
+    },
 ]
 
 POLICY_CLASSES = [
-    {"name": "Administrator", "class_id": 1, "description": "", "password_storage":2, "recovery_policy":3, "permanent_login":1},
-    {"name": "Public user", "class_id": 2, "description": "", "password_storage":2, "recovery_policy":3, "permanent_login":1},
-    {"name": "Legacy user", "class_id": 3, "description": "", "password_storage":2, "recovery_policy":3, "permanent_login":1}
+    {
+        "name": "Administrator",
+        "class_id": 1,
+        "description": "",
+        "password_storage": 2,
+        "recovery_policy": 3,
+        "permanent_login": 1,
+    },
+    {
+        "name": "Public user",
+        "class_id": 2,
+        "description": "",
+        "password_storage": 2,
+        "recovery_policy": 3,
+        "permanent_login": 1,
+    },
+    {
+        "name": "Legacy user",
+        "class_id": 3,
+        "description": "",
+        "password_storage": 2,
+        "recovery_policy": 3,
+        "permanent_login": 1,
+    },
 ]
 
 
 def categories() -> List[models.CategoryDef]:
     """Generate data for current arXiv categories."""
     return [
-        models.CategoryDef(
-            category=category,
-            name=data.full_name,
-            active=1
-        ) for category, data in CATEGORIES.items()
+        models.CategoryDef(category=category, name=data.full_name, active=1)
+        for category, data in CATEGORIES.items()
     ]
 
 
@@ -223,57 +260,57 @@ def create_all_legacy_db(test_db_file: str=DEV_SQLITE_FILE, echo: bool=False, ur
     return engine, url, test_db_file
 
 
-def bootstrap_db(output_jwt: bool=False, db_uri = f"sqlite:///{DEV_SQLITE_FILE}", jwt_secret: str = settings.JWT_SECRET):
+def bootstrap_db(
+    db_uri=f"sqlite:///{DEV_SQLITE_FILE}", jwt_secret: str = settings.JWT_SECRET
+):
     """Creates db if it does not exist, load standard data to tables, create
     fake users.
 
     This script will wait for the DB to become available and then check if the
     arXiv_submissions table exists.
 
-    If the table exists it will not make tables or load data. If `output_jwt` is
-    `True` it will make a auth session in the db for the first user and write to
-    stdout a JWT. NOTE that this will alter an existing DB by only maknig a auth
-    session.
+    If the table exists it will not make tables or load data.
 
     If the table doesn't exist, this script will create all the legacy tables,
-    add standard data like licenses, create several testing users. If
-    `output_jwt` is `True` it will make a auth session in the db for the first
-    user and write to stdout a JWT.
+    add standard data like licenses, create several testing users.
 
-    If it makes a auth session the session will be 1 year long so it can be
-    reused by devs.
+    The JWT will have an auth session for 1 year long so it can be reused by
+    devs.
 
     Ex:
 
-        python tests/make_tests_db.py --output-single-jwt > jwt.txt
+        python tests/make_tests_db.py > jwt.txt
         INTEGRATION_JWT = $(cat jwt.txt) python -m submit.integration.test_integration
 
     Testing and debugging this script outside of docker can be done like:
 
        JWT_SECRET='X' CLASSIC_DATABASE_URI='sqlite:///tmpbootstrap.db.sqlite' python tests/make_tests_db.py
 
-    ARGS:
-        output_jwt: bool Write to stdout a jwt of a user on completion of script
     """
     logger.setLevel(logging.DEBUG)
 
     from arxiv.config import settings
+
     settings.CLASSIC_DB_URI = db_uri
 
     app = Flask("bootstrap")
     app.url_map.strict_slashes = False
     app.config["JWT_SECRET"] = jwt_secret
-    logger.debug(f'JWT_SECRET: {app.config["JWT_SECRET"]}')
+    logger.debug(f"JWT_SECRET: {app.config['JWT_SECRET']}")
     app.config.from_object(settings)
     Base(app)
     Auth(app)
 
     from arxiv.db import init as db_init
+
     db_init(settings)  # only setups connection, does not make tables
     engine = _engine(db_uri, False)
 
     with app.app_context():
-        app.config['SESSION_DURATION']=30758400  # a year, make this as long as you want.
+        app.config["SESSION_DURATION"] = (
+            30758400  # a year, make this as long as you want.
+        )
+
         def user_to_jwt(user, auths):
             session = create(auths, "127.0.0.1", "localhost", "", user)
             return tokens.encode(session, app.config["JWT_SECRET"])
@@ -281,11 +318,14 @@ def bootstrap_db(output_jwt: bool=False, db_uri = f"sqlite:///{DEV_SQLITE_FILE}"
         with Session(engine) as session:
             wait_for_db(session, app)
             if engine.dialect.has_table(engine.connect(), "arXiv_submissions"):
-                logger.info("arXiv_submissions table already exists, DB bootstraped. No new users created.")
-                if output_jwt:
-                    jwt = user_to_jwt(session.query(models.TapirUser).first())
-                    print("\n")
-                return ""
+                logger.info(
+                    "arXiv_submissions table already exists, DB bootstraped. No new users created."
+                )
+                tu= session.query(models.TapirUser).first()
+                if not tu:
+                    raise RuntimeError("No users in already existing db")
+                return user_to_jwt(accounts.get_user_by_id(str(tu.user_id)), DEFAULT_AUTHS)
+
 
             logger.info("Database for classic not yet initialized; creating all tables")
             models.metadata.create_all(engine)
@@ -308,28 +348,45 @@ def bootstrap_db(output_jwt: bool=False, db_uri = f"sqlite:///{DEV_SQLITE_FILE}"
             created_users = []
             for user, pw, ip, host, endos in users_to_add:
                 new_user, auths = accounts.register(user, pw, ip, host)
+                if auths != DEFAULT_AUTHS:
+                    raise (
+                        "Authorizatoins created with accounts.register() differ from DEFAULT_AUTHS"
+                        "They should be the same. Either update the DEFAULT_AUTHS or figure "
+                        "out why the auths are different."
+                    )
+
                 for cat in endos:
                     if cat.in_archive == cat.id:  # it's an archive, ex nucl-th
-                        session.add(models.Endorsement(endorsee_id=new_user.user_id,
-                                                   archive=cat.in_archive,
-                                                   subject_class="",
-                                                   flag_valid=1, type="auto", point_value=10,
-                                                   issued_when=11074371513))
+                        session.add(
+                            models.Endorsement(
+                                endorsee_id=new_user.user_id,
+                                archive=cat.in_archive,
+                                subject_class="",
+                                flag_valid=1,
+                                type="auto",
+                                point_value=10,
+                                issued_when=11074371513,
+                            )
+                        )
                     else:
                         sc = cat.id.split(".")[1] if "." in cat.id else cat.id
-                        session.add(models.Endorsement(endorsee_id=new_user.user_id,
-                                                   archive=cat.in_archive,
-                                                   subject_class=sc,
-                                                   flag_valid=1, type="auto", point_value=10,
-                                                   issued_when=11074371513))
+                        session.add(
+                            models.Endorsement(
+                                endorsee_id=new_user.user_id,
+                                archive=cat.in_archive,
+                                subject_class=sc,
+                                flag_valid=1,
+                                type="auto",
+                                point_value=10,
+                                issued_when=11074371513,
+                            )
+                        )
                 created_users.append((new_user, auths))
             logger.info("Added %i users for testing", len(users_to_add))
             session.commit()
 
             jwt = user_to_jwt(created_users[0][0], created_users[0][1])
-            if output_jwt:
-                print(jwt)
-            return ""
+            return str(jwt)
 
 
 if __name__ == "__main__":
