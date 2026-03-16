340acb3b1c898eb1abf45405df924183b4e440c9
David Fielding <dlf2@cornell.edu>
2026-03-16 13:42:23 -0400

    Remove import of authorship.

--- submit_ce/ui/controllers/__init__.py
+++ submit_ce/ui/controllers/__init__.py
@@ -10,7 +10,6 @@ from ..backend import get_submission
 from . import cross, delete, jref, util, withdraw
 from .manage_submissions import manage_submissions
 from .new import process, upload, review
-from .new.authorship import authorship
 from .new.classification import classification
 from .new.create import create
 from .new.final import finalize
