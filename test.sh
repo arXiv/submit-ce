a5c90245be6185c7c549e0297bd0a8ca17c0fc40
Brian Maltzan <bgm37@cornell.edu>
2026-05-05 11:02:08 -0400

    ignore pubsub in automated tests

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@
 # all coverage settings configured in pyproject.toml
 uv run pytest \
    --cov=submit_ce \
+   --ignore=submit_ce/implementations/pubsub \
    submit_ce/api \
    submit_ce/implementations \
    submit_ce/domain \
