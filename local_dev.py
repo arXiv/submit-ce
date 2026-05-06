627dea7cbf5494a8478b66e3e777fab2efc776f0
Brian Maltzan <bgm37@cornell.edu>
2026-05-06 12:46:33 -0400

    config for tex2pdf api

--- local_dev.py
+++ local_dev.py
@@ -18,6 +18,7 @@ os.environ.setdefault('STORE_GS_BUCKET', 'arxiv-submit-dev')
 os.environ.setdefault('STORE_GS_PREFIX', DEV_NAME or getpass.getuser())
 os.environ.setdefault('GCLOUD_PROJECT', 'arxiv-development')
 os.environ.setdefault('TEMPLATES_AUTO_RELOAD', '1')
+os.environ.setdefault('COMPILE_API_URL', 'http://localhost:9001')
 
 from submit_ce.ui.factory import create_web_app
 
