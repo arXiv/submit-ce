1245ed2b459d0dd1ef70671df6cd68d9ac32cada
Brian D. Caruso <bdc34@cornell.edu>
2026-05-13 11:03:01 -0400

    Reorg of gs_file_store and legacy_file_store

--- submit_ce/implementations/file_store/gs_file_store.py
+++ submit_ce/implementations/file_store/gs_file_store.py
@@ -8,6 +8,7 @@ import json
 import io
 import logging
 import tarfile
+from typing_extensions import override
 
 from arxiv.files import FileObj, FileDoesNotExist
 from arxiv.files.object_store import GsObjectStore
@@ -67,13 +68,6 @@ class GsFileStore(SubmissionFileStore, FileStoreMixin):
         self.bucket = self.storage_client.bucket(self.gs_bucket)
         self.obj_store = GsObjectStore(self.bucket)
 
-    def __repr__(self) -> str:
-        return (f"{self.__class__.__name__}("
-                f"gs_bucket={self.gs_bucket},"
-                f"gs_prefix={self.gs_prefix},"
-                f"source_prefix={self.source_prefix},"
-                )
-
     def _blob_to_file_status(self, submission_id: str, blob) -> FileStatus:
         src_dir = self._source_path(submission_id)
         anc_dir = src_dir / "anc"
@@ -90,18 +84,24 @@ class GsFileStore(SubmissionFileStore, FileStoreMixin):
                           errors=[]) # TODO not sure where to get errors from
 
 
+    @override
     def get_source_file_info(self, submission_id: str, path: Path|str) -> FileStatus:
-        blob = self.bucket.get_blob(str(self._source_path(submission_id) / path))
+        get_path = self._source_path(submission_id) / path
+        self._check_path_safe(submission_id, get_path)
+        blob = self.bucket.get_blob(get_path)
         if blob is None:
             raise FileNotFoundError(f"File {path} does not exist in source for submission {submission_id}")
         return self._blob_to_file_status(submission_id, blob)
 
+    @override
     def delete_source_file(self, submission_id: str, path: Path|str) -> None:
-        blob = self.bucket.get_blob(str(self._source_path(submission_id) / path))
+        del_path = str(self._source_path(submission_id) / path)
+        self._check_path_safe(submission_id, del_path)
+        blob = self.bucket.get_blob(del_path)
         if blob is not None:
             blob.delete()
 
-
+    @override
     def get_workspace(self, submission_id: str, upload_id="fake") -> Workspace:
         src_dir = self._source_path(submission_id)
         files: List[FileStatus] = []
@@ -123,15 +123,19 @@ class GsFileStore(SubmissionFileStore, FileStoreMixin):
             errors=[]
         )
 
+    @override
     def store_source_file(self,
                      submission_id: str,
                      content: SubmitFile,
                      chunk_size: int) -> FileStatus:
         """Stores a file for a submisison."""
-        blob = self.bucket.blob(str(self._source_path(submission_id) / content.filename))
+        store_at = str(self._source_path(submission_id) / content.filename)
+        self._check_path_safe(submission_id, store_at)
+        blob = self.bucket.blob(store_at)
         blob.upload_from_file(content.stream, content_type=content.content_type)
         return self._blob_to_file_status(submission_id, blob)
 
+    @override
     def store_source_package(self,
                      submission_id: str,
                      content: SubmitFile,
@@ -155,30 +159,37 @@ class GsFileStore(SubmissionFileStore, FileStoreMixin):
                 if not member.isfile():
                     continue
                 with tar.extractfile(member) as file:
-                    blob = self.bucket.blob(str(src_dir / member.name))
+                    store_at = str(src_dir / member.name)
+                    self._check_path_safe(submission_id, store_at)  # TODO this will be strange, what to do?
+                    blob = self.bucket.blob(store_at)
                     blob.upload_from_file(file, size=member.size)
                     files.append( {"file":member.name, "bytes": member.size})
 
         return files
 
+    @override
     def get_preview(self, submission_id: str) -> FileObj:
         preview_path = self._preview_path(submission_id)
+        self._check_path_safe(submission_id, preview_path)
         preview = self.bucket.blob(str(preview_path))
         if preview.exists():
             return preview
         else:
             return FileDoesNotExist(str(preview_path))
 
+    @override
     def store_preview(self, submission_id: str,
                       content: IO[bytes],
                       chunk_size: int = 4096) -> str:
         """Store a preview PDF for a submission."""
         preview_path = self._preview_path(submission_id)
+        self._check_path_safe(submission_id, preview_path)
         blob = self.bucket.blob(preview_path)
         blob.upload_from_file(content)
         blob.reload()
         return blob.crc32c
 
+    @override
     def store_directives(self, submission_id: str, content: dict) -> str:
         """Store directives.json for a submission."""
         directives_path = self._directives_path(submission_id)
@@ -188,24 +199,65 @@ class GsFileStore(SubmissionFileStore, FileStoreMixin):
         blob.reload()
         return blob.crc32c
 
+    @override
     def get_source_checksum(self, submission_id: str) -> str:
         """Get the checksum of the source package for a submission."""
         return self._get_checksum(self._source_package_path(submission_id))
 
+    @override
     def does_source_exist(self, submission_id: str) -> bool:
         """Determine whether source has been deposited for a submission."""
         return self.bucket.blob(str(self._source_package_path(submission_id))).exists()
 
+    @override
     def get_preview_checksum(self, submission_id: str) -> str:
         """Get the checksum of the preview PDF for a submission."""
         return self._get_checksum(self._preview_path(submission_id))
 
+    @override
     def does_preview_exist(self, submission_id: str) -> bool:
         """Determine whether a preview has been deposited for a submission."""
         return self.bucket.blob(str(self._preview_path(submission_id))).exists()
 
-    def _get_checksum(self, path: Path) -> str:
-        item = self.bucket.blob(str(path))
+    @override
+    def get_source_file(self, submission_id: str, path: Path|str) -> FileObj:
+        src_path = str(self._source_path(submission_id) / path)
+        self._check_path_safe(submission_id, src_path)
+        blob = self.bucket.blob(src_path)
+        if blob.exists():
+            return blob
+        else:
+            return FileDoesNotExist(str(src_path))
+
+    @override
+    def get_source_pacakge_checksum(self, submission_id: str) -> str:
+        return self.get_source_checksum(submission_id)
+
+    @override
+    def delete_workspace(self, submission_id: str):
+        blobs = self.bucket.list_blobs(prefix=str(self._source_path(submission_id)))
+        for blob in blobs:
+            blob.delete()
+
+    @override
+    def is_available(self) -> bool:
+        """Determine whether the filesystem is available."""
+        try:
+            return self.bucket.exists()
+        except Exception as ex:
+            logger.error(f"Could not check if bucket exists: {ex}")
+            return False
+
+    @override
+    def delete_all_source_files(self, submission_id: str) -> None:
+        pass
+
+    @override
+    def delete_preview(self, submission_id: str) -> None:
+        pass
+
+    def _get_checksum(self, path: str) -> str:
+        item = self.bucket.blob(path)
         return item.crc32c
 
     def _submission_path(self, submission_id: str) -> Path:
@@ -215,7 +267,7 @@ class GsFileStore(SubmissionFileStore, FileStoreMixin):
 
     def _source_path(self, submission_id: str) -> Path:
         """Get the source path for the submission_id"""
-        return self._submission_path(submission_id) / self.source_prefix
+        return self._submission_path(str(submission_id)) / self.source_prefix
 
     def _source_package_path(self, submission_id: str) -> Path:
         return self._submission_path(submission_id) / f'{submission_id}.tar.gz'
@@ -229,6 +281,7 @@ class GsFileStore(SubmissionFileStore, FileStoreMixin):
     def _directives_path(self, submission_id: str) -> Path:
         return self._submission_path(submission_id) / 'directives.json'
 
+    @override
     def get_source_file(self, submission_id: str, path: Path|str) -> FileObj:
         src_path = self._source_path(submission_id) / path
         blob = self.bucket.blob(str(src_path))
@@ -237,6 +290,7 @@ class GsFileStore(SubmissionFileStore, FileStoreMixin):
         else:
             return FileDoesNotExist(str(src_path))
 
+    @override
     def get_preflight(self, submission_id: str) -> FileObj:
         preflight_path = self._preflight_path(submission_id)
         blob = self.bucket.blob(str(preflight_path))
@@ -245,6 +299,7 @@ class GsFileStore(SubmissionFileStore, FileStoreMixin):
         else:
             return FileDoesNotExist(str(preflight_path))
 
+    @override
     def get_directives(self, submission_id: str) -> FileObj:
         directives_path = self._directives_path(submission_id)
         blob = self.bucket.blob(str(directives_path))
@@ -253,12 +308,24 @@ class GsFileStore(SubmissionFileStore, FileStoreMixin):
         else:
             return FileDoesNotExist(str(directives_path))
 
+    @override
     def get_source_package_checksum(self, submission_id: str) -> str:
         return self.get_source_checksum(submission_id)
 
+    def _check_path_safe(self, submission_id: int|str, path: str|Path) -> None:
+        """Checks if a path is safely part of the files for `submission_id`.
+
+        Raises an error if the path is not under the `self._source_path()` for
+        `submission_id`
+        """
+        if not str(path).startswith(str(self._source_path(submission_id))):
+            raise RuntimeError(f"Path {path} not part of submission_id {submission_id}")
+
+    @override
     def delete_workspace(self, submission_id: str):
         raise RuntimeError("delete_workspace not implementated")
 
+    @override
     def is_available(self) -> bool:
         """Determine whether the filesystem is available."""
         try:
@@ -270,38 +337,52 @@ class GsFileStore(SubmissionFileStore, FileStoreMixin):
     def _full_base_path(self):
         return f'gs://{self.gs_bucket}'
 
+    @override
     def get_full_source_package_path(self, submission_id: str) -> str:
         return f'{self._full_base_path()}/{self._source_package_path(submission_id)}'
 
+    @override
     def get_full_preflight_package_path(self, submission_id: str) -> str:
         return f'{self._full_base_path()}/{self._preflight_path(submission_id)}'
 
+    @override
     def get_full_directives_package_path(self, submission_id: str) -> str:
         return f'{self._full_base_path()}/{self._directives_path(submission_id)}'
 
+    @override
     def delete_all_source_files(self, submission_id: str) -> None:
         blobs = self.bucket.list_blobs(prefix=str(self._source_path(submission_id)))
         for blob in blobs:
             blob.delete()
 
+    @override
     def delete_preview(self, submission_id: str) -> None:
         preview_path = self._preview_path(submission_id)
         blob = self.bucket.blob(str(preview_path))
         if blob.exists():
             blob.delete()
 
+    @override
     def delete_preflight(self, submission_id: str) -> None:
         blob = self.bucket.blob(str(self._preflight_path(submission_id)))
         if blob.exists():
             blob.delete()
 
+    @override
     def delete_directives(self, submission_id: str) -> None:
         blob = self.bucket.blob(str(self._directives_path(submission_id)))
         if blob.exists():
             blob.delete()
 
+    @override
     def store_zzrm(self, submission_id: str, content: dict) -> None:
         path = self._source_path(submission_id) / '00README.json'
         blob = self.bucket.blob(str(path))
         data = json.dumps(content).encode('utf-8')
         blob.upload_from_file(io.BytesIO(data), content_type='application/json')
+
+    def __repr__(self) -> str:
+        return (f"{self.__class__.__name__}("
+                f"gs_bucket={self.gs_bucket},"
+                f"gs_prefix={self.gs_prefix},"
+                f"source_prefix={self.source_prefix}")
---
+++ submit_ce/implementations/file_store/legacy_file_store.py
@@ -0,0 +1,323 @@
+import os
+import re
+import shutil
+from datetime import datetime, timezone
+from pathlib import Path
+from typing import IO, List
+from subprocess import Popen
+from hashlib import md5
+from base64 import urlsafe_b64encode
+from typing_extensions import override
+
+from arxiv.files import FileObj, LocalFileObj, FileDoesNotExist
+
+from submit_ce.api import SubmissionFileStore
+from submit_ce.domain import  Workspace
+from submit_ce.domain.uploads import UploadLifecycleStates, UploadStatus, FileStatus
+from submit_ce.domain.types import SubmitFile
+
+
+class SecurityError(RuntimeError):
+    """Something suspicious happened."""
+
+
+class UserFile:
+    pass
+
+
+class LegacyFileStore(SubmissionFileStore):
+    """
+    Functions for storing and getting source files from the legacy /data/new filesystem.
+
+    In the legacy system we use a shared volume. Inside of that, the first four digits of the ID we'll call a
+    "shard id". The shard id is used to create a directory that in turn holds a directory for each id's files.
+
+    For example, id ``65393829`` would have a directory at
+    ``{LEGACY_FILESYSTEM_ROOT}/6539/65393829``.
+
+    The directory contains:
+     - a PDF that was compiled,
+     - a ``source.log`` file (for the admins to look at)
+     - a ``src`` directory that contains the actual file content.
+
+    We also require the ability to set permissions on files and directories, and
+    set the owner user and group.
+
+    To use this, the following config parameters must be set:
+
+    - ``LEGACY_FILESYSTEM_ROOT``: (see above)
+    - ``LEGACY_FILESYSTEM_SOURCE_DIR_MODE``: permissions for directories; see
+      :ref:`python:os.chmod`
+    - ``LEGACY_FILESYSTEM_SOURCE_MODE``: permissions for files; see
+      :ref:`python:os.chmod`
+    - ``LEGACY_FILESYSTEM_SOURCE_UID``: uid for owner user (must exist)
+    - ``LEGACY_FILESYSTEM_SOURCE_GID``: gid for owner group (must exist)
+    - ``LEGACY_FILESYSTEM_SOURCE_PREFIX``
+
+    Adapted from NG arxiv-submission-core 2024-09-19. Changed to a class, use of Pathlib.
+    """
+
+    def __init__(self,
+                 root_dir: Path,
+                 source_file_mode = 0o42775,
+                 source_dir_mode = 0o42775,
+                 source_uid = os.geteuid(),
+                 source_gid = os.getegid(),
+                 source_prefix = "src"
+                 ):
+        self.root_dir = root_dir
+        """Path to the root directory of the file store shards."""
+        self.source_file_mode = source_file_mode
+        """Permission mode for files."""
+        self.source_dir_mode = source_dir_mode
+        """Permission mode for directories."""
+        self.source_uid = source_uid
+        """uid for owner user (must exist)."""
+        self.source_gid = source_gid
+        """gid for owner group (must exist)."""
+        self.source_prefix = source_prefix
+        """Prefix in the {root}/{shard}/{id} directory to store the source."""
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(root_dir={self.root_dir})"
+
+    @override
+    def get_source_file(self, submission_id: str, path: Path | str) -> FileObj:
+        src_path = self._source_path(submission_id) / path
+        if src_path.exists():
+            return LocalFileObj(src_path)
+        else:
+            return FileDoesNotExist(str(src_path))
+
+    @override
+    def get_source_file_info(self, submission_id: str, path: Path | str) -> FileStatus:
+        pass
+
+    @override
+    def store_source_file(self, submission_id: str, content: SubmitFile, chunk_size: int) -> FileStatus:
+        return super().store_source_file(submission_id, content, chunk_size)
+
+    @override
+    def get_source_pacakge_checksum(self, submission_id: str) -> str:
+        pass
+
+    @override
+    def get_preview(self, submission_id: str) -> FileObj:
+        path = self._preview_path(submission_id)
+        if path.exists():
+            return LocalFileObj(path)
+        else:
+            return FileDoesNotExist(path.name)
+
+    @override
+    def is_available(self) -> bool:
+        """Determine whether the filesystem is available."""
+        return os.path.exists(self.root_dir)
+
+    @override
+    def get_workspace(self, submission_id: str, upload_id: str) -> Workspace:
+        src_dir = self._source_path(str(submission_id))
+        anc_dir = (src_dir / "anc")
+        files: List[FileStatus] = []
+        for path in Path(self._source_path(str(submission_id))).rglob("*"):
+            stat = path.stat()
+            files.append(FileStatus(path=str(path.relative_to(src_dir)),
+                                    name=path.name,
+                                    content_type="unknown",
+                                    bytes=stat.st_size,
+                                    modified=datetime.fromtimestamp(stat.st_mtime, tz=timezone.utc),
+                                    url="http://example.com/fakeurl",
+                                    crc32c="fakecrc32",
+                                    ancillary=anc_dir in path.parent.parents,
+                                    is_versioned=False
+                                    ))
+
+        return Workspace(
+            identifier=str(submission_id),
+            checksum='fake-checksum-asdf1234',
+            size=sum([file.bytes for file in files]),
+            started=datetime.now(),
+            completed=datetime.now(),
+            created=datetime.now(),
+            modified=datetime.now(),
+            status=UploadStatus.READY,
+            lifecycle=UploadLifecycleStates.ACTIVE,
+            locked=False,
+            files=files,
+            errors=[]
+        )
+
+    @override
+    def delete_source_file(self, submission_id: str, path: Path|str) -> None:
+        pass
+
+    @override
+    def delete_workspace(self, submission_id: str):
+        src_dir = self._source_path(submission_id)
+        shutil.rmtree(src_dir.absolute())
+
+    @override
+    def store_source_package(self,
+                     submission_id: str,
+                     content: SubmitFile,
+                     chunk_size = 4096) -> str:
+        """Store a source package for a submission."""
+        # Make sure that we have a place to put the source files.
+        package_path = self._source_package_path(submission_id)
+        source_path = self._source_path(submission_id)
+        if not os.path.exists(package_path):
+            os.makedirs(os.path.split(package_path)[0])
+        if not os.path.exists(source_path):
+            os.makedirs(source_path)
+
+        ["application/x-gzip", "application/gzip", "application/tar",
+                    "application/x-tar", "application/tar+gzip",]
+        with open(package_path, 'wb') as f:
+            while True:
+                chunk = content.read(chunk_size)
+                if not chunk:
+                    break
+                f.write(chunk)
+
+        self._unpack_tarfile(package_path, source_path)
+        self._set_modes(package_path)
+        self._set_modes(source_path)
+        return self.get_source_checksum(submission_id)
+
+    @override
+    def store_preview(self, submission_id: str, content: IO[bytes],
+                      chunk_size: int = 4096) -> str:
+        """Store a preview PDF for a submission."""
+        preview_path = self._preview_path(submission_id)
+        preview_dir = os.path.split(preview_path)[0]
+        if not os.path.exists(preview_dir):
+            os.makedirs(preview_dir)
+        with open(preview_path, 'wb') as f:
+            while True:
+                chunk = content.read(chunk_size)
+                if not chunk:
+                    break
+                f.write(chunk)
+        self._set_modes(preview_path)
+        return self.get_preview_checksum(submission_id)
+
+    @override
+    def get_source_checksum(self, submission_id: str) -> str:
+        """Get the checksum of the source package for a submission."""
+        return self._get_checksum(self._source_package_path(submission_id))
+
+    @override
+    def does_source_exist(self, submission_id: str) -> bool:
+        """Determine whether source has been deposited for a submission."""
+        return os.path.exists(self._source_package_path(submission_id))
+
+    @override
+    def get_preview_checksum(self, submission_id: str) -> str:
+        """Get the checksum of the preview PDF for a submission."""
+        return self._get_checksum(self._preview_path(submission_id))
+
+    @override
+    def does_preview_exist(self, submission_id: str) -> bool:
+        """Determine whether a preview has been deposited for a submission."""
+        return self._preview_path(submission_id).exists()
+
+    @override
+    def delete_all_source_files(self, submission_id: str) -> None:
+        pass
+
+    @override
+    def delete_preview(self, submission_id: str) -> None:
+        pass
+
+    def _well_formed_submission_id(self, submission_id: str) -> None:
+        """Checkt that submission_id is okay."""
+        if len(str(submission_id)) > 32 or not re.match(r'^\d+', str(submission_id)):
+            raise SecurityError('Submission ID is improperly typed. This is a security concern.')
+
+    def _submission_path(self, submission_id: str) -> Path:
+        """Gets classic filesystem structure is such as /{rootdir}/{first 4 digits of submission id}/{submission id}"""
+        self._well_formed_submission_id(submission_id)
+        shard_dir = self.root_dir / Path(str(submission_id)[:4])
+        return shard_dir / Path(str(submission_id))
+
+    def _source_path(self, submission_id: str) -> Path:
+        """Get the source path for the submission_id"""
+        return self._submission_path(submission_id) / self.source_prefix
+
+    def _source_package_path(self, submission_id: str) -> Path:
+        return self._submission_path(submission_id) / f'{submission_id}.tar.gz'
+
+    def _preview_path(self, submission_id: str) -> Path:
+        return self._submission_path(submission_id) / f'{submission_id}.pdf'
+
+    def _get_checksum(self, path: str) -> str:
+        hash_md5 = md5()
+        with open(path, "rb") as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+                hash_md5.update(chunk)
+        return urlsafe_b64encode(hash_md5.digest()).decode('utf-8')
+
+    def _unpack_tarfile(self, tar_path: str, unpack_to: str) -> None:
+        result = Popen(['tar', '-xzf', tar_path, '-C', unpack_to]).wait()
+        if result != 0:
+            raise RuntimeError(f'tar exited with {result}')
+
+    def _chmod_recurse(self, parent: Path, dir_mode: int, file_mode: int,
+                      uid: int, gid: int) -> None:
+        """
+        Recursively chmod and chown all directories and files.
+
+        Parameters
+        ----------
+        parent : str
+            Root directory for the operation (included).
+        dir_mode : int
+            Mode to set directories.
+        file_mode : int
+            Mode to set files.
+        uid : int
+            UID for chown.
+        gid : int
+            GID for chown.
+
+        """
+        if not os.path.isdir(parent):
+            os.chown(parent, uid, gid)
+            os.chmod(parent, file_mode)
+            return
+
+        for path, directories, files in os.walk(parent):
+            for directory in directories:
+                os.chown(os.path.join(path, directory), uid, gid)
+                os.chmod(os.path.join(path, directory), dir_mode)
+            for fname in files:
+                os.chown(os.path.join(path, fname), uid, gid)
+                os.chmod(os.path.join(path, fname), file_mode)
+        os.chown(parent, uid, gid)
+        os.chmod(parent, dir_mode)
+
+    def _set_modes(self, path: str) -> None:
+        dir_mode = self.source_dir_mode
+        file_mode = self.source_file_mode
+        source_uid = self.source_uid
+        source_gid = self.source_gid
+        self._chmod_recurse(path, dir_mode, file_mode, source_uid, source_gid)
+
+    def _check_safe(self, workspace: Workspace, full_path: str,
+                    is_ancillary: bool = False, is_removed: bool = False,
+                    is_persisted: bool = False, is_system: bool = False,
+                    strict: bool = True) -> None:
+        if not strict or is_system:
+            wks_full_path = self.get_path_bare(workspace.base_path,
+                                               is_persisted=is_persisted)
+        elif is_ancillary:
+            wks_full_path = self.get_path_bare(workspace.ancillary_path,
+                                               is_persisted=is_persisted)
+        elif is_removed:
+            wks_full_path = self.get_path_bare(workspace.removed_path,
+                                               is_persisted=is_persisted)
+        else:
+            wks_full_path = self.get_path_bare(workspace.source_path,
+                                               is_persisted=is_persisted)
+        if wks_full_path not in full_path:
+            raise ValueError(f'Not a valid path for workspace: {full_path}')
