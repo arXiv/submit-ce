4a84afc6177849ed098afc70d9ed6ec6761c4797
Brian D. Caruso <bdc34@cornell.edu>
2026-05-21 08:56:30 -0400

    Changes to mock file store and move to its own file
    Claude authored

--- submit_ce/ui/conftest.py
+++ submit_ce/ui/conftest.py
@@ -71,7 +71,7 @@ def mocked_compile_service(app: Flask) -> None:
 
 def mocked_file_store(app: Flask) -> None:
     """Alter `app.api` to use a MockFileStore that accepts uploads in tests."""
-    from submit_ce.implementations import MockFileStore
+    from submit_ce.implementations.file_store.mock_file_store import MockFileStore
     app.api.store = MockFileStore()
 
 
