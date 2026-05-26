56669ea2187761066fe52617e7dad85b14c44178
Brian D. Caruso <bdc34@cornell.edu>
2026-05-26 16:08:48 -0400

    Adds file changes needed for Withdraw event
    Claude authored

--- submit_ce/domain/event/legacy.py
+++ submit_ce/domain/event/legacy.py
@@ -2,9 +2,11 @@
 
 
 from pydantic import Field
+from submit_ce.api.submit import SubmitApi
 from submit_ce.domain.event.base import EventWithSideEffect
 from submit_ce.domain.exceptions import InvalidEvent
 from submit_ce.domain.submission import Submission
+from submit_ce.domain.uploads import InMemorySubmitFile
 
 
 class Withdraw(EventWithSideEffect):
@@ -23,9 +25,19 @@ class Withdraw(EventWithSideEffect):
         if not submission.is_announced:
             raise InvalidEvent(self, "Submission must already be announced")
 
+    def execute(self, api: SubmitApi, submission: Submission) -> None:
+        """Make the file for the withdraw."""
+        content = '%auto-ignore'
+        file_store = api.get_file_store()
+        withdrawn_file = InMemorySubmitFile("withdrawn", content.encode("utf-8"))
+        file_store.delete_all_source_files(str(submission.submission_id))
+        file_store.store_source_file(str(submission.submission_id),
+                                     withdrawn_file, chunk_size=4096)
+
     def project(self, submission: Submission) -> Submission:
         """Update the submission status and withdrawal reason."""
         assert self.created is not None
+        submission.source_format = "withdrawn"
         submission.is_source_processed = True
         submission.type = 'wdr'
         submission.source_format = 'withdrawn'
